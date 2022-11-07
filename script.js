13d7a0521ac67600236fdc2e808806ea3fd05aae
kmortal <kelvynmoraes2004@gmail.com>
2022-11-07 05:31:12 +0000

    fix bug of responsive menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector('ul')
 
 function openMenu(){
-    menu.style.height = menu.style.height == '90vh' ? '0' : '90vh'
+    menu.style.height = menu.style.height == '50vh' ? '0' : '50vh'
 }
